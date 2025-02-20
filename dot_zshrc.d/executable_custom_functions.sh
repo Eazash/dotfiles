34c2ef10c6847b2e42e72db946611090b26fe30f
Ezira Ashenafi <eazash22@gmail.com>
2025-02-20 11:42:13 +0300

    feat: add aws helper functions to remove arguments from docker_ecr_login

--- dot_zshrc.d/executable_custom_functions.sh
+++ dot_zshrc.d/executable_custom_functions.sh
@@ -20,7 +20,19 @@ export -f updateEBENV >/dev/null 2>&1
 function ssh_host_to_ip { ssh -G $1 | grep -iw hostname | cut -d ' ' -f 2 | tr -d '\n' | xargs echo}
 export -f ssh_host_to_ip >/dev/null 2>&1
 
+# Get AWS Account ID
+function aws_account_id { aws sts get-caller-identity | jq -r '.Account' }
+export -f aws_account_id >/dev/null 2>&1
+
+# Get AWS Default Region
+function aws_default_region { aws ec2 describe-availability-zones | jq -r '.AvailabilityZones[0].RegionName' }
+export -f aws_default_region >/dev/null 2>&1
+
+# Get ECR URL
+function aws_ecr_url { echo "$(aws_account_id).dkr.ecr.$(aws_default_region).amazonaws.com" }
+export -f aws_ecr_url >/dev/null 2>&1
+
 # AWS ECR Login
-function docker_ecr_login { aws ecr get-login-password --region $2 | docker login --username AWS --password-stdin "$1.dkr.ecr.$2.amazonaws.com" }
+function docker_ecr_login { aws ecr get-login-password --region "$(aws_default_region)" | docker login --username AWS --password-stdin "$(aws_ecr_url)" }
 export -f docker_ecr_login >/dev/null 2>&1
 
